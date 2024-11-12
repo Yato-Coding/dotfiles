030066cffa9200ee8526aea365a5af8507fe7c07
Yato-Coding <maximus17435@gmail.com>
2024-11-12 23:28:45 +0100

    bang

---
+++ init.lua
@@ -0,0 +1,61 @@
+require("config.lazy")
+require('leap').create_default_mappings()
+
+-- tab indent on select
+vim.keymap.set('v', '<tab>', '>gv', { noremap = true, silent = true })
+vim.keymap.set('v', '<S-tab>', '>gv', { noremap = true, silent = true })
+
+-- g
+vim.keymap.set('n', '<leader>g', ':noh<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'g*', '*Ncgn', { noremap = true })
+
+-- ctrl-c/ctrl-v
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-v>', '"+p', { noremap = true, silent = true })
+
+-- Highlighting
+vim.keymap.set('n', '<leader>he', ':HSExport<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>hi', ':HSImport<CR>', {noremap = true, silent = true})
+vim.keymap.set('v', '<leader>hl', ':<c-u>HSHighlight ')
+vim.keymap.set('v', '<leader>nl', ':<c-u>HSRmHighlight<CR>')
+
+-- Path completion with custom source command
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+  function() 
+      require("fzf-lua").complete_path() 
+  end,
+  { silent = true, desc = "Fuzzy complete path" })
+
+-- dirs
+vim.keymap.set('n', '<leader>`', ':cd %:p:h<CR>', { noremap = true, silent = true})  
+
+-- buffers
+vim.keymap.set('n', '<leader>bb', ':b#<CR>', { noremap = true, silent = true })  
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })  
+vim.keymap.set('n', '<leader>bn', ':bn<CR>', { noremap = true, silent = true })  
+vim.keymap.set('n', '<leader>bp', ':bp<CR>', { noremap = true, silent = true })  
+
+-- telescope
+vim.keymap.set('n', '<leader>fc', ':Telescope<CR>', { desc = 'Telescope' })
+vim.keymap.set('n', '<leader>fa', ':Telescope agrolens query=functions', { desc = 'Telescope agrolens' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Telescope help tags' })
+
+-- oil
+vim.keymap.set('n', '<leader>o', ':Oil<CR>', {noremap = true, silent = true})
+
+-- colorscheme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+-- disable the ugly ass gutter
+vim.diagnostic.config({
+    virtual_text = true,  -- Set to true to show virtual text instead
+    signs = false,
+    underline = true,
+    update_in_insert = false,
+})
+
+
---
+++ lua/config/lazy.lua
@@ -0,0 +1,194 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+vim.opt.number = true          -- Enable line numbers
+vim.opt.relativenumber = true  -- Enable relative line numbers
+vim.opt.tabstop = 4            -- Set tab width to 4 spaces
+vim.opt.shiftwidth = 4         -- Set indentation width to 4 spaces
+vim.opt.expandtab = true       -- Use spaces instead of tabs
+
+-- Setup lazy.nvim
+require('lazy').setup({
+    spec = {
+        { 'ellisonleao/gruvbox.nvim', priority = 1000 , config = true},
+        { 'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make'
+        },
+        {'desdic/agrolens.nvim'},
+        {'nvim-telescope/telescope.nvim',
+            tag = '0.1.8',
+            dependencies = { 'nvim-lua/plenary.nvim' },
+            config = function ()
+                require('telescope').load_extension('fzf')
+                require('telescope').load_extension('agrolens')
+            end
+        },
+        { 'stevearc/oil.nvim',
+            dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+            config = function()
+                require('oil').setup({
+                    view_options = {
+                        show_hidden = true,  -- Show hidden files
+                    },
+                })
+            end,
+        },
+        {'goolord/alpha-nvim',
+            dependencies = { 'echasnovski/mini.icons' },
+            config = function ()
+                require'alpha'.setup(require'alpha.themes.startify'.config)
+            end
+        },
+        {'L3MON4D3/LuaSnip',
+            version = 'v2.*', 
+            build = 'make install_jsregexp',
+            dependencies = { 'rafamadriz/friendly-snippets' }
+        },
+        {'williamboman/mason.nvim',
+            config = function()
+                require('mason').setup {}
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim',
+            config = function ()
+                require('mason-lspconfig').setup_handlers {
+                    -- The first entry (without a key) will be the default handler
+                    -- and will be called for each installed server that doesn't have
+                    -- a dedicated handler.
+                    function (server_name) -- default handler (optional)
+                        require('lspconfig')[server_name].setup {
+                            capabilities = require('cmp_nvim_lsp').default_capabilities()
+                        }
+                    end
+                }
+                -- Next, you can provide a dedicated handler for specific servers.
+                -- For example, a handler override for the `rust_analyzer`:
+                --['rust_analyzer'] = function ()
+                --  require('rust-tools').setup {}
+                --end
+            end -- Closing function for config
+        },
+        {'neovim/nvim-lspconfig'},
+        {'hrsh7th/nvim-cmp',       -- Main completion plugin
+            dependencies = {
+                'hrsh7th/cmp-nvim-lsp',   -- LSP source for nvim-cmp
+                'hrsh7th/cmp-buffer',      -- Buffer completion source
+                'hrsh7th/cmp-path',        -- Path completion source
+                'hrsh7th/cmp-cmdline',     -- Command line completion source
+                'saadparwaiz1/cmp_luasnip',     -- LuaSnip for snippet support
+            },
+            config = function ()
+                local ls = require'luasnip'
+                require('luasnip.loaders.from_vscode').lazy_load()
+
+                local cmp = require'cmp'
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            ls.lsp_expand(args.body)
+                        end,
+                    },
+                    mapping = {
+                        ['<C-Space>'] = cmp.mapping.complete(),  -- Trigger completion
+                        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm completion
+                        ['<C-e>'] = cmp.mapping.close(), -- Close completion menu
+                        ['<tab>'] = cmp.mapping.select_next_item(), -- Next item
+                        ['<S-tab>'] = cmp.mapping.select_prev_item(), -- Previous item
+                    },
+                    sources = {
+                        { name = 'nvim_lsp' },  -- LSP completion
+                        { name = 'buffer' },     -- Buffer completion
+                        { name = 'path' },       -- Path completion
+                        { name = 'luasnip' }
+                    },
+                    completion = {
+                        completeopt = 'menu,menuone,noinsert',  -- Completion menu options
+                        keyword_length = 2,                     -- Minimum number of characters to trigger completion
+                    },
+                })
+
+                -- Setup cmdline completion
+                cmp.setup.cmdline(':', {
+                    sources = {
+                        { name = 'cmdline' }
+                    }
+                })
+            end
+        },
+        {'altermo/ultimate-autopair.nvim',
+            event={'InsertEnter','CmdlineEnter'},
+            branch='v0.6', --recommended as each new version will have breaking changes
+            opts={
+                --Config goes here
+            }
+        },
+        {'nvim-treesitter/nvim-treesitter',
+            build = ':TSUpdate',
+            config = function ()
+                require'nvim-treesitter.configs'.setup {
+                    highlight = {
+                        enable = true,    -- Enable syntax highlighting
+                        disable = {},     -- Optionally, disable for specific languages
+                    },
+                    incremental_selection = {
+                        enable = true,
+                        keymaps = {
+                            node_incremental = 'v',
+                            node_decremental = 'V',
+                        },
+                    },
+                    indent = { enable = true },
+                    folding = { enable = true }
+                }
+            end,
+        },
+        {'nvim-lualine/lualine.nvim',
+            dependencies = {'nvim-tree/nvim-web-devicons'},
+            opts = { theme = 'gruvbox' },  -- Changed 'options' to 'opts'
+            config = function()
+                require('lualine').setup({})
+            end,
+        },
+        {'ggandor/leap.nvim',
+            dependencies = {'tpope/vim-repeat'},
+            config = function ()
+                require('leap').create_default_mappings()
+            end,
+        },
+        {'Pocco81/HighStr.nvim',
+            config = function ()
+                require('high-str').setup({
+                    saving_path = '/tmp/highstr/'
+                })
+            end
+        }
+    },
+
+
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { 'gruvbox' } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
+
